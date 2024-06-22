e71383a7010055c275dd46a5a7f033d97a43281d
Ironpatriot06 <147107745+Ironpatriot06@users.noreply.github.com>
2024-06-22 13:52:03 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,35 @@
+let input=document.getElementById("input");
+let buttons=document.querySelectorAll("button");
+
+let str = "";
+let arr= Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener("click", (e) =>{
+        if(e.target.innerHTML == "="){
+            str=eval(str);
+            input.value=str;
+        }
+        // else if(e.target.innerHTML == "-" ){
+        //     if(e.target.innerHTML =="-"){
+
+        //         str="ERROR";
+        //         input.value=str;
+        //     }else{
+        //         null;
+        //     }
+        // }
+        else if(e.target.innerHTML == "AC"){
+            str="";
+            input.value=str;
+        }
+        else if(e.target.innerHTML== "DEL"){
+            str=str.substring(0, str.length-1);
+            input.value=str;
+        }
+        else{
+             str += e.target.innerHTML;
+            input.value =str;
+        }
+    })
+})
+
